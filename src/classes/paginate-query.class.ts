d6900bd44e0e5d51f96beffe0f5fb6015c856afe
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-11-02 12:55:26 +0000

    Minor fixes and tweaks to Swagger definitions

--- src/classes/paginate-query.class.ts
+++ src/classes/paginate-query.class.ts
@@ -1,4 +1,4 @@
-import { IsNumber, IsObject } from "class-validator";
+import { IsArray, IsNumber, IsOptional } from "class-validator";
 import { ApiProperty } from "@nestjs/swagger";
 import {
     OrderBy,
@@ -10,48 +10,52 @@ import {
 
 export class PaginateQuery implements PaginateQueryInterface {
     @IsNumber()
+    @IsOptional()
     @ApiProperty({
         name: "page",
         required: false,
         type: Number,
         description:
-            "The page number to retrieve.  If the query value is higher than the number of pages, the last page is returned.  If not provided or less than 1, the first page is returned."
+            "The page number to retrieve.  If the query value is higher than the number of pages, the last page is returned.  If not provided or less than 1, the first page is returned.",
+        example: 2
     })
     page?: number;
 
     @IsNumber()
+    @IsOptional()
     @ApiProperty({
         name: "limit",
         required: false,
         type: Number,
-        description: "The number of items to retrieve per page.  Will be capped at a server-set maximum value if higher"
+        description: "The number of items to retrieve per page.  Will be capped at a server-set maximum value if higher",
+        example: 10
     })
     limit?: number;
 
-    @IsObject()
+    @IsArray()
+    @IsOptional()
     @ApiProperty({
         name: "orderBy",
         required: false,
         isArray: true,
         type: Array<OrderByClause>,
         example: [
-            ["name", "ASC"],
-            ["groupId", "DESC"]
+            ["id", "ASC"]
         ],
         description:
             "The order by clause to sort the results by; either 'ASC' or 'DESC'. If not provided, no sort is set."
     })
     orderBy?: OrderBy;
 
-    @IsObject()
+    @IsArray()
+    @IsOptional()
     @ApiProperty({
         name: "where",
         required: false,
         isArray: true,
         type: Array<WhereClause>,
         example: [
-            ["groupId", "12"],
-            ["active", "true"]
+            ["id", "12"],
         ],
         description:
             "The where clause to filter the results by.  For each field listed, an AND operation is performed for equality against the values provided.  If no where clause is provided, all results are fetched."
