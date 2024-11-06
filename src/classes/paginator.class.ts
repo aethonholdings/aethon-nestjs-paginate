20b9855b6595d7440b2192357843f1fb2a49bbf1
Konstantinos Dimitriou <konstantinos.dimitriou@aethon.sg>
2024-11-06 15:22:24 +0000

    Removed getters and setters for simplicity

--- src/classes/paginator.class.ts
+++ src/classes/paginator.class.ts
@@ -16,14 +16,14 @@ type PaginationParameters = {
 };
 
 export class Paginator {
-    private _config: PaginateConfig;
-    private _query: PaginateQuery;
-    private _path: string;
+    config: PaginateConfig;
+    query: PaginateQuery;
+    path: string;
 
     constructor(config: PaginateConfig, query: PaginateQuery, path: string) {
-        this._config = config;
-        this._query = query;
-        this._path = path;
+        this.config = config;
+        this.query = query;
+        this.path = path;
     }
 
     run<T>(source: T[] | Promise<T[]> | Repository<T>): Promise<Paginated<T>> {
@@ -39,7 +39,7 @@ export class Paginator {
                     relations: {}
                 };
                 // add where clauses to the find options
-                this._query.where?.forEach((whereClause: WhereClause) => {
+                this.query.where?.forEach((whereClause: WhereClause) => {
                     findOptions.where = {
                         ...findOptions.where,
                         [whereClause[0]]: whereClause[1]
@@ -51,17 +51,16 @@ export class Paginator {
                 // the result is then passed on as a promise to be packaged
                 result = source.findAndCount(findOptions).then((totalItems) => {
                     // add relations to the find options
-                    this._config.relations?.forEach((relationship) => {
+                    this.config.relations?.forEach((relationship) => {
                         findOptions.relations[relationship] = true;
                     });
                     // add order by clauses to the find options
-                    this._query.orderBy?.forEach((orderBy: OrderByClause) => {
+                    this.query.orderBy?.forEach((orderBy: OrderByClause) => {
                         findOptions.order[orderBy[0]] = orderBy[1];
                     });
                     paginationParams = this._getPaginationParams(totalItems[1]);
                     findOptions.skip = paginationParams.startOffset;
                     findOptions.take = paginationParams.itemsPerPage;
-                    console.log(findOptions);
                     return (source as Repository<T>).find(findOptions);
                 });
             } else {
@@ -72,7 +71,7 @@ export class Paginator {
                 // the source is now a promise; chain the promise to get the total items and the paginated data
                 result = source
                     .then((data) => {
-                        return this._query.where ? this._where(data) : data;
+                        return this.query.where ? this._where(data) : data;
                     })
                     // order by id ASC, if present, because by default TypeORM orders by id
                     // this step ensures that array-based data source results match the repository-based results
@@ -80,7 +79,7 @@ export class Paginator {
                         return data.sort((a, b) => a["id"] - b["id"]);
                     })
                     .then((data) => {
-                        return this._query.orderBy ? this._orderBy(data) : data;
+                        return this.query.orderBy ? this._orderBy(data) : data;
                     })
                     .then((data) => {
                         paginationParams = this._getPaginationParams(data.length);
@@ -93,43 +92,16 @@ export class Paginator {
         });
     }
 
-    getQuery(): PaginateQuery {
-        return this._query;
-    }
-
-    setQuery(query: PaginateQuery): this {
-        this._query = query;
-        return this;
-    }
-
-    getPath(): string {
-        return this._path;
-    }
-
-    setPath(): this {
-        this._path = this._path;
-        return this;
-    }
-
-    getConfig(): PaginateConfig {
-        return this._config;
-    }
-
-    setConfig(config: PaginateConfig): this {
-        this._config = config;
-        return this;
-    }
-
     private _package<T>(data: T[], paginationParams: PaginationParameters): Paginated<T> {
         let append: string = "";
         append =
             append +
-            (this._query.orderBy && this._query.orderBy.length
-                ? `&orderBy=${JSON.stringify(this._query.orderBy)}`
+            (this.query.orderBy && this.query.orderBy.length
+                ? `&orderBy=${JSON.stringify(this.query.orderBy)}`
                 : "");
         append =
             append +
-            (this._query.where && this._query.where.length ? `&where=${JSON.stringify(this._query.where)}` : "");
+            (this.query.where && this.query.where.length ? `&where=${JSON.stringify(this.query.where)}` : "");
         const response: Paginated<T> = {
             meta: {
                 itemsPerPage: paginationParams.itemsPerPage,
@@ -139,28 +111,28 @@ export class Paginator {
             },
             data: data.map((obj) => obj as T),
             links: {
-                first: `${this._path}?page=1&limit=${paginationParams.itemsPerPage}${append}`,
-                current: `${this._path}?page=${paginationParams.currentPage}&limit=${paginationParams.itemsPerPage}${append}`,
-                last: `${this._path}?page=${paginationParams.totalPages}&limit=${paginationParams.itemsPerPage}${append}`
+                first: `${this.path}?page=1&limit=${paginationParams.itemsPerPage}${append}`,
+                current: `${this.path}?page=${paginationParams.currentPage}&limit=${paginationParams.itemsPerPage}${append}`,
+                last: `${this.path}?page=${paginationParams.totalPages}&limit=${paginationParams.itemsPerPage}${append}`
             }
         };
-        if (this._query.orderBy) response.meta.orderBy = this._query.orderBy;
-        if (this._query.where) response.meta.where = this._query.where;
+        if (this.query.orderBy) response.meta.orderBy = this.query.orderBy;
+        if (this.query.where) response.meta.where = this.query.where;
         paginationParams.currentPage > 1
-            ? (response.links.previous = `${this._path}?page=${paginationParams.currentPage - 1}&limit=${paginationParams.itemsPerPage}${append}`)
+            ? (response.links.previous = `${this.path}?page=${paginationParams.currentPage - 1}&limit=${paginationParams.itemsPerPage}${append}`)
             : null;
         paginationParams.currentPage < paginationParams.totalPages
-            ? (response.links.next = `${this._path}?page=${paginationParams.currentPage + 1}&limit=${paginationParams.itemsPerPage}${append}`)
+            ? (response.links.next = `${this.path}?page=${paginationParams.currentPage + 1}&limit=${paginationParams.itemsPerPage}${append}`)
             : null;
         return response;
     }
 
     private _getPaginationParams(totalItems: number): PaginationParameters {
-        let itemsPerPage: number = this._query.limit || this._config.limit;
-        if (this._query.limit < 1) itemsPerPage = this._config.limit;
-        if (this._query.limit > this._config.limitMax) itemsPerPage = this._config.limitMax;
+        let itemsPerPage: number = this.query.limit || this.config.limit;
+        if (this.query.limit < 1) itemsPerPage = this.config.limit;
+        if (this.query.limit > this.config.limitMax) itemsPerPage = this.config.limitMax;
         const totalPages = Math.ceil(totalItems / itemsPerPage) || 1;
-        const currentPage = Math.max(Math.min(this._query.page || 1, totalPages), 1);
+        const currentPage = Math.max(Math.min(this.query.page || 1, totalPages), 1);
         const startOffset = (currentPage - 1) * itemsPerPage;
         const endOffset = Math.min(startOffset + itemsPerPage, totalItems) - 1;
 
@@ -169,8 +141,8 @@ export class Paginator {
             totalItems: totalItems,
             currentPage: currentPage,
             totalPages: totalPages,
-            orderBy: this._query.orderBy || this._config.orderBy,
-            where: this._query.where,
+            orderBy: this.query.orderBy || this.config.orderBy,
+            where: this.query.where,
             startOffset: startOffset,
             endOffset: endOffset
         };
@@ -179,7 +151,7 @@ export class Paginator {
     private _orderBy<T>(data: T[]): T[] {
         return data.sort((a, b) => {
             let sort: number = 0;
-            for (const orderByClause of this._query.orderBy) {
+            for (const orderByClause of this.query.orderBy) {
                 if (a[orderByClause[0]] instanceof String) {
                     sort =
                         orderByClause[1] === "ASC"
@@ -198,7 +170,7 @@ export class Paginator {
 
     private _where<T>(data: T[]): T[] {
         return data.filter((item) => {
-            for (const whereClause of this._query.where) {
+            for (const whereClause of this.query.where) {
                 if (item[whereClause[0]] != whereClause[1]) return false;
             }
             return true;
